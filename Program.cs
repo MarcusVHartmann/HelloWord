e28f5f7d41ce77b9fa63304a6abfda5ef518d542
MarcusVHart <haykirengeral@gmail.com>
2025-03-17 11:16:03 -0300

    Descrição da atividade da faculdade

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+console.WriteLine("Primeira linha de codigos, sendo a primeira atividade da aula de Algoritmos da Fatec Rubens Lara - Baixada Santista")
